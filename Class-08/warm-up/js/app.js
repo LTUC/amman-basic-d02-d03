dd9179397e85ed0f29343416bf63e437f7faa508
Sajanader <sajanader93@gmail.com>
2023-05-18 02:45:13 +0300

    feat:add warm up excersice

---
+++ Class-08/warm-up/js/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+alert("hello world";
+
+
+const NumberClassStudents = 50 
+
+let arr = ['razan','saja','haimouni', 'eman','rawan','tahani'];
+
+function printInstructorName() {
+
+    for (let i = 0; i <= arr; i++) {
+        console.log(arr[x]);    
+    }
+}
+
+NumberClassStudents = 40;
+
+
