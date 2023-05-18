afce8917aebaaeb27ab39d222afc6585d3d53553
Sajanader <sajanader93@gmail.com>
2023-05-18 13:47:33 +0300

    feat: finish the demo

--- Class-08/warm-up/js/app.js
+++ Class-08/warm-up/js/app.js
@@ -1,19 +1,20 @@
 'use strict'
 
-alert("hello world";
+alert("hello world");
 
 
-const NumberClassStudents = 50 
+const NumberClassStudents = 50;
 
 let arr = ['razan','saja','haimouni', 'eman','rawan','tahani'];
 
 function printInstructorName() {
 
-    for (let i = 0; i <= arr; i++) {
-        console.log(arr[x]);    
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);    
     }
 }
 
-NumberClassStudents = 40;
+printInstructorName();
+
 
 
